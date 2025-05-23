ab18f085246b7705f718c1019883c15bff9b03c3
Drolpg <leandronascimento.drolpg21@gmail.com>
2025-05-23 10:56:21 -0300

    update folder structure

--- src/schemas/client_schema.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, AwareDatetime
-
-
-class Clients(BaseModel):
-    id: int
-    name: str
-    email: str
-    number_fone: str
-    date_register: AwareDatetime | None = None
